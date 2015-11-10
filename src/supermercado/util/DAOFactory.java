f05b6ed783331ae4020ce8001e0317f9b6427179
braynner <max.braynner@gmail.com>
2015-11-10 20:18:50 -0300

    Refatoração do DAO e melhoria no polimorfismo

--- src/supermercado/util/DAOFactory.java
+++ src/supermercado/util/DAOFactory.java
@@ -1,20 +1,12 @@
 package supermercado.util;
 
-import supermercado.cliente.ClienteDAO;
-import supermercado.cliente.ClienteDAOHibernate;
-
 /**
- * Classe onde estão todas as conexões realizadas no banco de dados.
- * Usada para diminuir o acoplamento na camada de acesso a dados.
- * @author Matheus Protázio
- *
+ * Classe responsável por fabricar Daos
  */
 public class DAOFactory {
 	
-	// Cria uma conexão com banco de dados para ClienteDAO
-	public static ClienteDAO criarClienteDAO() {
-		ClienteDAOHibernate clienteDAO = new ClienteDAOHibernate();
-		clienteDAO.setSession(HibernateUtil.getSessionfactory().getCurrentSession());
-		return clienteDAO;
+	public Object getDao(Class clas) throws InstantiationException, IllegalAccessException{
+		return clas.newInstance();
 	}
+	
 }
