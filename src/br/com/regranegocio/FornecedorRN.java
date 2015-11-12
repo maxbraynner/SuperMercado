9987a2c4281d718ef9fae257912b12ebed984fc9
braynner <max.braynner@gmail.com>
2015-11-12 20:59:52 -0300

    adição do método excluir e correção no método salvar

--- src/br/com/regranegocio/FornecedorRN.java
+++ src/br/com/regranegocio/FornecedorRN.java
@@ -14,14 +14,23 @@ public class FornecedorRN {
 	public FornecedorRN() throws InstantiationException, IllegalAccessException { 
 		DAOFactory factory = new DAOFactory(); 
 		
+		// recupera o dao
 		this.fornedorDAO = (FornecedorDAO) factory.getDao(FornecedorDAO.class);
 	}
 	
 	public void salvar(Fornecedor fornecedor) {
-		fornedorDAO.inserir(fornecedor);
+		if (fornecedor.getId() == null || fornecedor.getId().intValue() == 0) {
+			fornedorDAO.inserir(fornecedor);
+		}else{
+			fornedorDAO.alterar(fornecedor);
+		}
 	}
 	
 	public List<Fornecedor> listar() {
 		return fornedorDAO.listar();
+	}
+	
+	public void excluir(Fornecedor fornecedor){
+		this.fornedorDAO.remover(fornecedor);
 	}
 }
