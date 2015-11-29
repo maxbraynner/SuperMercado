4f81330d288ea4506df6bc24b52391787a309ca1
Elvis Martins <Elvis Martins@ElvisMartins-PC>
2015-11-29 10:53:34 -0200

    classe de mensagem global

---
+++ src/br/com/util/JSFUtil.java
@@ -0,0 +1,20 @@
+package br.com.util;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+
+public class JSFUtil {
+
+	public static void adicionarMensagemSucesso(String mensagem){
+		FacesMessage msgm = new FacesMessage(FacesMessage.SEVERITY_INFO, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msgm);
+	}
+	
+	public static void adicionarMensagemErro(String mensagem){
+		FacesMessage msgm = new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagem, mensagem);
+		FacesContext contexto = FacesContext.getCurrentInstance();
+		contexto.addMessage(null, msgm);
+	}
+	
+}
