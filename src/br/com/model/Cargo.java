846bef67e8ed442766e1269c1268d142f3eba02b
braynner <max.braynner@gmail.com>
2015-11-28 21:35:44 -0300

    Remoção do atributo data de contratação

--- src/br/com/model/Cargo.java
+++ src/br/com/model/Cargo.java
@@ -1,7 +1,5 @@
 package br.com.model;
 
-import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -19,13 +17,9 @@ public class Cargo {
 	
 	private String nome;
 
-	@Column(precision = 2)
 	private double salario;
 	
 	private String setor;
-	
-	@Column(name = "data_contratacao", nullable = false, updatable = false)
-	private Date dataContratacao;
 	
 	public String getNome() {
 		return nome;
@@ -51,14 +45,6 @@ public class Cargo {
 		this.setor = setor;
 	}
 
-	public Date getDataContratacao() {
-		return dataContratacao;
-	}
-
-	public void setDataContratacao(Date dataContratacao) {
-		this.dataContratacao = dataContratacao;
-	}
-
 	public Integer getId() {
 		return id;
 	}
@@ -71,7 +57,6 @@ public class Cargo {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((dataContratacao == null) ? 0 : dataContratacao.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
 		long temp;
@@ -90,11 +75,6 @@ public class Cargo {
 		if (getClass() != obj.getClass())
 			return false;
 		Cargo other = (Cargo) obj;
-		if (dataContratacao == null) {
-			if (other.dataContratacao != null)
-				return false;
-		} else if (!dataContratacao.equals(other.dataContratacao))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
