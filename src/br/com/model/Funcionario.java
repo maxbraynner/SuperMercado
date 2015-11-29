d2af280e8781c02f87c6cb7ed44774117a7c2858
braynner <max.braynner@gmail.com>
2015-11-29 19:08:03 -0300

    alteração na funcionalidade de gerente

--- src/br/com/model/Funcionario.java
+++ src/br/com/model/Funcionario.java
@@ -6,15 +6,10 @@ import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.Cascade;
-import org.hibernate.annotations.CascadeType;
 
 @Entity
 @Table(name = "funcionario")
@@ -27,20 +22,16 @@ public class Funcionario extends Pessoa {
 	@Column(unique = true, nullable = false)
 	private String matricula;
 
-	// Coleção com os funcionários que determina funcionário gerencia
-	@OneToMany(fetch = FetchType.EAGER)
-	@JoinColumn(name = "funcionarios_gerenciados", nullable = true)
-	@org.hibernate.annotations.ForeignKey(name = "fk_funcionario_gerenciados")
-	private Set<Funcionario> gerenciados = new HashSet<Funcionario>();
+	@ManyToOne
+	private Funcionario gerente;
 
-	// todos os cargos que o funcionário já exerceu
 	@ManyToOne
 	@JoinColumn(name = "id_cargo")
 	@org.hibernate.annotations.ForeignKey(name = "fk_funcionario_cargo")
 	private Cargo cargo;
 	
 	// Vendas realizadas pelo funcionário
-	@OneToMany(fetch = FetchType.EAGER, mappedBy = "funcionario")
+	@OneToMany(mappedBy = "funcionario")
 	private Set<Venda> vendas = new HashSet<Venda>(0);
 	
 	@Column(name = "data_contratacao", nullable = false, updatable = false)
@@ -60,14 +51,6 @@ public class Funcionario extends Pessoa {
 
 	public void setMatricula(String matricula) {
 		this.matricula = matricula;
-	}
-
-	public Set<Funcionario> getGerenciados() {
-		return gerenciados;
-	}
-
-	public void setGerenciados(Set<Funcionario> gerenciados) {
-		this.gerenciados = gerenciados;
 	}
 
 	public Set<Venda> getVendas() {
@@ -92,6 +75,14 @@ public class Funcionario extends Pessoa {
 
 	public void setCargo(Cargo cargo) {
 		this.cargo = cargo;
+	}
+
+	public Funcionario getGerente() {
+		return gerente;
+	}
+
+	public void setGerente(Funcionario gerente) {
+		this.gerente = gerente;
 	}
 
 	@Override
