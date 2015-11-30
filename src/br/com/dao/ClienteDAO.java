05f055e95aca4b3badab6321b01efcf4a92d21ad
braynner <max.braynner@gmail.com>
2015-11-30 00:46:22 -0300

    Método consultar por cpf adicionado

--- src/br/com/dao/ClienteDAO.java
+++ src/br/com/dao/ClienteDAO.java
@@ -1,5 +1,6 @@
 package br.com.dao;
 
+import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -15,5 +16,23 @@ public class ClienteDAO extends DaoGeneric<Cliente> {
 	@Autowired
 	public ClienteDAO(SessionFactory sesseionFactory) {
 		super(Cliente.class, sesseionFactory);
+	}
+	
+	public Cliente consultaPorCPF(String cpf) {
+		String consulta = " select cliente.ativo, cliente.matricula, pessoa.*"
+				+ " from supermercado.cliente as cliente"
+				+ " join supermercado.pessoa as pessoa on pessoa.id = cliente.id "
+				+ " where pessoa.cpf = :cpf";
+		
+		SQLQuery sqlQuery = this.getSessionFactory().getCurrentSession().createSQLQuery(consulta);
+		sqlQuery.setParameter("cpf", cpf);
+		
+		Object[] result = (Object[]) sqlQuery.uniqueResult();
+		
+		Cliente cliente = new Cliente();
+		
+		cliente = consultarPorID(Integer.parseInt(result[2].toString()));
+		
+		return cliente;
 	}
 }
